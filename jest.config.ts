68763b749e1bb81b97d914596480229ef6a73b8c
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-05-21 20:40:52 +0300

    fix: standardize import statements and formatting in jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,13 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -96,7 +96,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -202,5 +204,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default createJestConfig(config)
-
+export default createJestConfig(config);
