18de6b88b67a521c563fa8f0c7c716abce9781be
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-05-21 18:17:59 +0300

    feat: add FormFieldError styled component for displaying error messages

---
+++ src/presentation/ui/FormFieldError.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FormFieldError = styled.p`
+  color: red;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 12px;
+  vertical-align: middle;
+`;
