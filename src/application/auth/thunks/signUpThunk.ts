9214940c9855cdab5a4b762364737605b4aae256
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-05-22 13:31:29 +0300

    feat: enhance signUpThunk with improved error handling and add corresponding unit tests

--- src/application/auth/thunks/signUpThunk.ts
+++ src/application/auth/thunks/signUpThunk.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { AuthPayload } from "../types/types";
 import { authService } from "@/infrastructure/api/authService";
 import { AUTH_SIGNUP } from "@/application/constants/actionTypes";
-import { AppError, AppErrorProps } from "@/shared/errors/types";
+import { AppError, AppErrorProps, AppErrorType } from "@/shared/errors/types";
 
 export const signUpThunk = createAsyncThunk<
   void,
@@ -16,6 +16,9 @@ export const signUpThunk = createAsyncThunk<
     if (error instanceof AppError) {
       return rejectWithValue(error.toPlain());
     }
-    throw error;
+    return rejectWithValue({
+          type: AppErrorType.UNKNOWN,
+          message: (error as Error).message,
+        });
   }
 });
