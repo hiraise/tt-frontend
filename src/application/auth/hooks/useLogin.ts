5e0514ef832e8438dcc0404d51cd2e207b10931b
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-05-23 17:05:44 +0300

    feat: implement middleware for route protection and user authentication

--- src/application/auth/hooks/useLogin.ts
+++ src/application/auth/hooks/useLogin.ts
@@ -1,9 +1,11 @@
+import { useRouter, useSearchParams } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "sonner";
 
 import { loginThunk } from "../thunks/loginThunk";
 import { RootState, AppDispatch } from "@/infrastructure/redux/store";
 import { errorTexts, successTexts } from "@/shared/locales/messages";
+import { ROUTES } from "@/infrastructure/config/routes";
 
 interface LoginFormProps {
   email: string;
@@ -11,14 +13,19 @@ interface LoginFormProps {
 }
 
 export const useLogin = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const dispatch = useDispatch<AppDispatch>();
   const loading = useSelector((state: RootState) => state.auth.loading);
+
+  const from = searchParams.get("from") || ROUTES.dashboard;
 
   const login = async ({ email, password }: LoginFormProps) => {
     const thunk = loginThunk({ email, password });
     try {
       await dispatch(thunk).unwrap();
       toast.success(successTexts.loginSuccess);
+      router.replace(from);
     } catch (error) {
       //TODO: add log to sentry
       console.log(errorTexts.somethingWentWrong, error);
