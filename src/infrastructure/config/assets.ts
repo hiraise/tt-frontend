a49f304fe255ff6498156a9fbd32b7a55944de7c
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-05-28 14:40:20 +0300

    feat: add new icons and logo to assets configuration

--- src/infrastructure/config/assets.ts
+++ src/infrastructure/config/assets.ts
@@ -1,8 +1,15 @@
 export const ASSETS = {
   logo: {
+    mobileSimple: "/images/logo.png",
     mobile: "/images/logo-mobile.png",
   },
   icons:{
     image: "/icons/image.png",
+    search: "/icons/iconamoon_search-bold.png",
+    profile: "/icons/iconamoon_profile.png",
+    board: "/icons/circum_view-board.png",
+    project: "/icons/codicon_github-project.png",
+    task: "/icons/hugeicons_task-01.png",
+    archive: "/icons/material-symbols-light_archive-outline.png",
   }
 };
