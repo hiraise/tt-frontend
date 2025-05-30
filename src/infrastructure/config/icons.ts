5dc820ad9f1cf06b390d643de5300c55e4ad6dd0
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-05-30 12:08:56 +0300

    feat: reorganize asset management by importing SVG icons and removing unused PNG icons

---
+++ src/infrastructure/config/icons.ts
@@ -0,0 +1,15 @@
+import SearchIcon from "../../../public/icons/iconamoon_search-bold.svg";
+import ProfileIcon from "../../../public/icons/iconamoon_profile.svg";
+import ProjectIcon from "../../../public/icons/codicon_github-project.svg";
+import BoardIcon from "../../../public/icons/circum_view-board.svg";
+import TaskIcon from "../../../public/icons/hugeicons_task-01.svg";
+import ArchiveIcon from "../../../public/icons/material-symbols-light_archive-outline.svg";
+
+export const ICONS = {
+  search: SearchIcon,
+  profile: ProfileIcon,
+  project: ProjectIcon,
+  board: BoardIcon,
+  task: TaskIcon,
+  archive: ArchiveIcon,
+};
