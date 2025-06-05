05ad965c20365f89bc08b5b9e7ebcf8e4c050148
Ali Musaev <musaev.ae@modul.tech>
2025-06-05 13:21:29 +0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: process.env.NEXT_PUBLIC_ROOT_PATH,
+  trailingSlash: true,
 };
 
 export default nextConfig;
