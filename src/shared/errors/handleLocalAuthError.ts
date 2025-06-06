f314ace11609498207cc852c33fdf70d96bf47e7
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-06-06 17:23:35 +0300

    fix: simplify error handling for resend verification by removing specific message checks

--- src/shared/errors/handleLocalAuthError.ts
+++ src/shared/errors/handleLocalAuthError.ts
@@ -45,17 +45,9 @@ export const localErrorHandlers: Record<
   resendVerification: (error) => {
     if (error instanceof AxiosError) {
       const status = error.response?.status;
-      const msg = error.response?.data?.msg;
       switch (status) {
         case 400:
-          if (msg === "request validation failed") {
-            return new AppError(AppErrorType.AUTH, "email format is incorrect");
-          } else if (msg === "user already verified") {
-            return new AppError(AppErrorType.AUTH, "user already verified");
-          }
-          return new AppError(AppErrorType.AUTH, errorTexts.invalidRequestBody);
-        case 401:
-          return new AppError(AppErrorType.AUTH, errorTexts.invalidCredentials);
+          return new AppError(AppErrorType.AUTH, errorTexts.somethingWentWrong);
         default:
           return null;
       }
