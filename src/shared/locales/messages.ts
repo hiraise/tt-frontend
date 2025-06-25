2579136cfe2685bf097bc9f922699c8873c9b34b
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-06-25 17:13:55 +0300

    feat(locales): add new success and error messages for authentication processes

--- src/shared/locales/messages.ts
+++ src/shared/locales/messages.ts
@@ -1,15 +1,18 @@
 export const errorTexts = {
   networkProblem: "Нет подключения к интернету",
   invalidCredentials: "Неверный логин или пароль",
-  invalidRequestBody: "Произошла непредвиденная ошибка. Обратитесь в службу поддержки",
+  invalidRequestBody:
+    "Произошла непредвиденная ошибка. Обратитесь в службу поддержки",
   userAlreadyExists: "Пользователь уже существует",
   sessionExpired: "Сессия истекла. Пожалуйста, войдите снова",
   serverError: "Ошибка сервера. Попробуйте позже",
   somethingWentWrong: "Что-то пошло не так. Попробуйте позже",
   unknownError: "Произошла неизвестная ошибка",
+  authenticationRequired: "Требуется вход в систему",
 };
 
 export const successTexts = {
   signUpSuccessCheckEmail: "Вы успешно зарегистрировались, проверьте почту",
   loginSuccess: "Вы успешно вошли в систему",
+  logoutSuccess: "Вы успешно вышли из системы",
 };
