06269602526f15719dbe4820aedc9207389da556
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-06-25 17:11:32 +0300

    feat(user): enhance UserInitializer to handle errors during current user retrieval

--- src/app/UserInitializer.tsx
+++ src/app/UserInitializer.tsx
@@ -4,12 +4,17 @@ import { useEffect } from "react";
 
 import { useAppDispatch } from "@/infrastructure/redux/hooks";
 import { getCurrentUserThunk } from "@/application/user/thunks/getCurrentUserThunk";
+import { clearState } from "@/application/user/slices/userSlice";
 
 export default function UserInitializer() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getCurrentUserThunk());
+    dispatch(getCurrentUserThunk())
+      .unwrap()
+      .catch(() => {
+        dispatch(clearState());
+      });
   }, [dispatch]);
 
   return null;
