2adb22aecdf94d9aae9f739aae05aa972149d9be
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-06-25 17:12:12 +0300

    refactor(apiRoutes): simplify API route definitions using constants for better maintainability

--- src/infrastructure/config/apiRoutes.ts
+++ src/infrastructure/config/apiRoutes.ts
@@ -1,9 +1,14 @@
+const API_VERSION = "/v1";
+const AUTH = `${API_VERSION}/auth`;
+const USERS = `${API_VERSION}/users`;
+
 export const API_ROUTES = {
-  LOGIN: "/v1/auth/login",
-  SIGNUP: "/v1/auth/register",
-  REFRESH: "/v1/auth/refresh",
-  AUTH_CHECK: "/v1/auth/check",
-  VERIFY: "/v1/auth/verify",
-  RESEND_VERIFICATION: "/v1/auth/resend-verification",
-  USER: (id: number) => `/v1/users/${id}`,
+  LOGIN: `${AUTH}/login`,
+  LOGOUT: `${AUTH}/logout`,
+  SIGNUP: `${AUTH}/register`,
+  REFRESH: `${AUTH}/refresh`,
+  AUTH_CHECK: `${AUTH}/check`,
+  VERIFY: `${AUTH}/verify`,
+  RESEND_VERIFICATION: `${AUTH}/resend-verification`,
+  USER: (id: number) => `${USERS}/${id}`,
 };
