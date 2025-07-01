affc4270bbb0df8591ab7b7a56251c4219307be0
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-07-01 21:57:58 +0300

    refactor(auth): remove unused thunk handling and simplify auth state management

--- src/application/auth/slices/authSlice.ts
+++ src/application/auth/slices/authSlice.ts
@@ -1,23 +1,11 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import {
-  checkAuthStatusThunk,
-  loginThunk,
-  logoutThunk,
-  signUpThunk,
-} from "../thunks/authThunks";
-import { handleAsyncThunkCases } from "@/shared/utils/handleAsyncThunkCases";
-
 export interface AuthState {
-  loading: boolean; // TODO: replace with authInitializing
-  error: string | null;
   isAuthenticated: boolean;
   authInitializing: boolean;
 }
 
 const initialState: AuthState = {
-  loading: false,
-  error: null,
   isAuthenticated: false,
   authInitializing: true,
 };
@@ -33,12 +21,6 @@ const authSlice = createSlice({
       state.authInitializing = action.payload;
     },
   },
-  // extraReducers: (builder) => {
-  //   handleAsyncThunkCases(builder, loginThunk);
-  //   handleAsyncThunkCases(builder, logoutThunk);
-  //   handleAsyncThunkCases(builder, signUpThunk);
-  //   handleAsyncThunkCases(builder, checkAuthStatusThunk);
-  // },
 });
 
 export const { setAuthenticated, setAuthInitializing } = authSlice.actions;
