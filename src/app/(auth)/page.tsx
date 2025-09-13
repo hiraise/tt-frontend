8238ee88e73aaa19efb87e8d37155b6f32639bc0
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-13 21:13:11 +0300

    feat: add login page styles and implement device-based rendering for desktop and mobile

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,29 +1,18 @@
-"use client";
+import styles from "./page.module.css";
 
-import { loginTexts } from "@/shared/locales/login";
-import { SectionTitle } from "@/presentation/widgets/common/SectionTitle";
-import { Spacer } from "@/presentation/widgets/primitives/Spacer";
-import { BottomLinks } from "@/presentation/widgets/common/BottomLinks";
-import { MobileLogo } from "@/presentation/ui/MobileLogo";
-import MainContainer from "@/presentation/widgets/primitives/MainContainer";
-import { ROUTES } from "@/infrastructure/config/routes";
-import { sharedTexts } from "@/shared/locales/sharedTexts";
-import { LoginForm } from "@/presentation/widgets/auth/LoginForm/LoginForm";
+import { DeviceBased } from "@/presentation/ui/DeviceBased";
+import { LoginDesktopPage, LoginMobilePage } from "@/presentation/pages/login";
 
 export default function LoginPage() {
+  // Temporary styles only for desktop page
   return (
-    <MainContainer>
-      <MobileLogo />
-      <Spacer size="56px" />
-      <SectionTitle title={loginTexts.title} subtitle={loginTexts.subtitle} />
-      <Spacer size="56px" />
-      <LoginForm />
-      <Spacer size="92px" />
-      <BottomLinks
-        href={ROUTES.signUp}
-        text={loginTexts.noAccount}
-        buttonText={sharedTexts.signUp}
-      />
-    </MainContainer>
+    <DeviceBased
+      desktop={
+        <div className={styles.root}>
+          <LoginDesktopPage />
+        </div>
+      }
+      mobile={<LoginMobilePage />}
+    />
   );
 }
