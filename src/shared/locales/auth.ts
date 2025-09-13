fe874c0a875d08ecea5f2aaa9b6435342ec34552
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-13 21:13:15 +0300

    feat: add authentication text localization for login and privacy policy

--- src/shared/locales/auth.ts
+++ src/shared/locales/auth.ts
@@ -4,4 +4,19 @@ export const authTexts = {
   changePassword: "Изменить пароль",
   changingPassword: "Изменение пароля",
   passwordPlaceholder: "********",
+  noAccount: "Нет аккаунта?",
+  signUp: "Зарегистрироваться",
+  login: {
+    title: "Вход",
+    description: "Введите почту и пароль\r\nдля входа в свою учетную запись",
+    forgotPassword: "Забыли пароль?",
+    login: "Войти",
+    loggingIn: "Вход в систему...",
+    passwordPlaceholder: "Пароль",
+  },
+  privacyPolicy: {
+    text: "Нажимая кнопку {btnName} вы соглашаетесь с ",
+    buttonText: "Политикой конфиденциальности",
+    buttonLink: "https://google.com",
+  },
 };
