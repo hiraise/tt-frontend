b8d1174305e4d37d37e7333c2c17b480c71fb0e5
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-14 13:19:49 +0300

    feat: refactor LoginDesktopPage to use new components and update styles for better layout

--- src/presentation/pages/login/LoginDesktopPage.tsx
+++ src/presentation/pages/login/LoginDesktopPage.tsx
@@ -1,23 +1,39 @@
 "use client";
 
 import Link from "next/link";
+import clsx from "clsx";
 
 import styles from "./LoginDesktopPage.module.css";
 
 import { authTexts } from "@/shared/locales/auth";
 import { ROUTES } from "@/infrastructure/config/routes";
-import { DesktopLogin } from "@/presentation/widgets/auth/Login";
+import { DESIGN_ELEMENTS } from "@/infrastructure/config/icons";
+import { Icon } from "@/presentation/ui/Icon";
+import { LoginFormDesktop } from "@/presentation/widgets/auth/LoginForm";
+import {
+  AuthBackground,
+  BottomContent,
+  TitleWrapper,
+} from "@/presentation/widgets/auth/_components";
 
 export function LoginDesktopPage() {
   return (
     <>
-      <DesktopLogin />
-      <div className={styles.bottomContent}>
-        <span className="body-reg-2">{authTexts.noAccount}</span>
+      <AuthBackground>
+        <Icon as={DESIGN_ELEMENTS.search} className={styles.search} />
+        <Icon as={DESIGN_ELEMENTS.task} className={styles.task} />
+        <TitleWrapper>
+          <h1>{authTexts.login.title}</h1>
+          <p className="body-reg">{authTexts.login.description}</p>
+        </TitleWrapper>
+        <LoginFormDesktop />
+      </AuthBackground>
+      <BottomContent>
+        <span className={clsx(styles.text, "body-reg-2")}>{authTexts.noAccount}</span>
         <Link href={ROUTES.signUp}>
-          <span className="btn-font-s">{authTexts.signUp}</span>
+          <span className="btn-font-s">{authTexts.signup.signup}</span>
         </Link>
-      </div>
+      </BottomContent>
     </>
   );
 }
