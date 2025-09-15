277a72e10c6df51fa0dfbedd007526db77fd6458
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-15 22:29:01 +0300

    feat: add localization texts for drawer component

---
+++ src/shared/locales/texts.ts
@@ -0,0 +1,11 @@
+export const TEXTS = {
+  drawer: {
+    myProjects: "Мои проекты",
+    myBoards: "Мои доски",
+    myTasks: "Мои задачи",
+    myProfile: "Мой профиль",
+    search: "Поиск",
+    collapse: "Свернуть",
+    expand: "Развернуть",
+  },
+};
