c2906d706ee4b86177755716f4bee076a2b9e79f
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-18 17:36:23 +0300

    feat: remove unused CSS styles and refactor ChangeTaskStatusModal for improved structure and clarity

--- src/presentation/widgets/modals/ChangeTaskStatusModal.tsx
+++ src/presentation/widgets/modals/ChangeTaskStatusModal.tsx
@@ -1,46 +1,49 @@
 "use client";
 
-import styles from "./ChangeTaskStatusModal.module.css";
+import { useState } from "react";
 
 import { BaseModal } from "./BaseModal";
 import { BaseModalProps } from "./BaseModal.types";
 import { TaskStatus } from "@/domain/task/task.entity";
-import { Icon } from "@/presentation/ui/Icon";
-import { ICONS } from "@/infrastructure/config/icons";
+import { TEXTS } from "@/shared/locales/texts";
+import { DeviceBased } from "@/presentation/ui/DeviceBased";
+import { TaskStatusDesktop, TaskStatusMobile } from "../tasks/ChangeTaskStatus";
 import { useGlobalModalProps } from "@/shared/hooks/useGlobalModalProps";
-import { useGetProjectStatuses } from "@/application/projects/hooks/useProject";
 import { ChangeStatusProps } from "@/shared/hooks/useGlobalModals";
 
-interface ChangeTaskStatusProps {
-  onChange: (newStatus: TaskStatus) => void;
-}
-
-export function ChangeTaskStatus({ onChange }: ChangeTaskStatusProps) {
-  const { currentStatus, projectId } = useGlobalModalProps<ChangeStatusProps>() ?? {};
-  const { data: statuses } = useGetProjectStatuses(projectId!);
-
-  return (
-    <div className={styles.statusContainer}>
-      {statuses?.map((status) => (
-        <div key={status.id} className={styles.statusWrapper} onClick={() => onChange(status)}>
-          {status.name}
-          {status.id === currentStatus?.id && (
-            <Icon as={ICONS.checkMark} size="14px" inheritColor />
-          )}
-        </div>
-      ))}
-    </div>
-  );
-}
-
 export default function ChangeTaskStatusModal(props: BaseModalProps<TaskStatus>) {
-  const handleSelectStatus = (newStatus: TaskStatus) => {
-    props.onClose(newStatus);
+  const { onClose, ...rest } = props;
+
+  const { currentStatus } = useGlobalModalProps<ChangeStatusProps>() ?? {};
+  const [status, setStatus] = useState(currentStatus);
+
+  const handleApply = () => onClose(status);
+  const handleClose = () => onClose(undefined);
+
+  const handleSelect = (value: TaskStatus, isDesktop?: boolean) => {
+    setStatus(value);
+    // Close modal immediately only on mobile devices
+    if (!isDesktop) onClose(value);
   };
 
   return (
-    <BaseModal title="Статус задачи" {...props}>
-      <ChangeTaskStatus onChange={handleSelectStatus} />
+    <BaseModal title={TEXTS.tasks.status} onClose={handleClose} {...rest}>
+      <DeviceBased
+        desktop={
+          <TaskStatusDesktop
+            selectedStatus={status}
+            onSelect={(value) => handleSelect(value, true)}
+            onApply={handleApply}
+            onClose={handleClose}
+          />
+        }
+        mobile={
+          <TaskStatusMobile
+            selectedStatus={status}
+            onSelect={(value) => handleSelect(value, false)}
+          />
+        }
+      />
     </BaseModal>
   );
 }
