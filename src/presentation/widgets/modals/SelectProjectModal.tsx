f12782bb6867cba78ce9769ec813b48ebd2f3a02
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-19 20:31:12 +0300

    feat: enhance SelectProjectModal with project selection state management and dialog buttons

--- src/presentation/widgets/modals/SelectProjectModal.tsx
+++ src/presentation/widgets/modals/SelectProjectModal.tsx
@@ -1,22 +1,40 @@
 "use client";
 
+import { useState } from "react";
+
 import { Project } from "@/domain/project/project.entity";
 import { BaseModal } from "./BaseModal";
 import { BaseModalProps } from "./BaseModal.types";
 import { ProjectsList } from "../tasks/ProjectsList";
 import { useGet } from "@/application/projects/hooks/useProject";
+import { DialogButtons } from "../common/DialogButtons";
+import { useGlobalModalProps } from "@/shared/hooks/useGlobalModalProps";
+import { TEXTS } from "@/shared/locales/texts";
 
 export default function SelectProjectModal(props: BaseModalProps<Project>) {
   const { data: projects = [] } = useGet();
+  const { projectId: initialProjectId } = useGlobalModalProps<{ projectId?: number }>() || {};
+  const [selectedProjectId, setSelectedProjectId] = useState<number | undefined>(initialProjectId);
 
   const handleOnSelect = (project: Project) => {
-    props.onClose(project);
+    setSelectedProjectId(project.id);
     console.log("Project selected:", project.name);
   };
 
+  const handleApply = () => {
+    const selectedProject = projects.find((p) => p.id === selectedProjectId);
+    props.onClose(selectedProject);
+  };
+  const handleClose = () => props.onClose(undefined);
+
   return (
-    <BaseModal {...props} fullScreen title="Проект">
-      <ProjectsList projects={projects} onSelect={handleOnSelect} />
+    <BaseModal {...props} fullScreen title={TEXTS.tasks.project}>
+      <ProjectsList
+        projects={projects}
+        onSelect={handleOnSelect}
+        selectedProjectId={selectedProjectId}
+      />
+      <DialogButtons onClose={handleClose} onApply={handleApply} />
     </BaseModal>
   );
 }
