18854f25ab2d1aa4f179f37eb5919c20a9e428dc
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-22 15:20:05 +0300

    feat: refactor MoveToArchiveModal to use ActionProps and improve item archiving functionality

--- src/presentation/widgets/modals/MoveToArchiveModal.tsx
+++ src/presentation/widgets/modals/MoveToArchiveModal.tsx
@@ -2,18 +2,20 @@ import { BaseModal } from "./BaseModal";
 import { BaseModalProps } from "./BaseModal.types";
 import { DeviceBased } from "@/presentation/ui/DeviceBased";
 import { useGlobalModalProps } from "@/shared/hooks/useGlobalModalProps";
-import { TaskActionProps } from "@/shared/hooks/useGlobalModals";
-import { MoveToArchive } from "../tasks/TaskActions";
+import { ActionProps } from "@/shared/hooks/useGlobalModals";
+import { MoveToArchive } from "../common/Actions";
 import { DialogButtons } from "../common/DialogButtons";
 
-export default function MoveToArchiveModal(props: BaseModalProps<string>) {
-  const { id, title } = useGlobalModalProps<TaskActionProps>() ?? {};
+export default function MoveToArchiveModal(props: BaseModalProps<number>) {
+  const { type, id, title } = useGlobalModalProps<ActionProps>() ?? {};
+
+  if (!type || !title) return;
 
   const handleClose = () => props.onClose();
 
+  // return item ID for archiving to parent component
   const handleMove = () => {
-    console.log("Move task with ID: ", id);
-    props.onClose();
+    if (id) props.onClose(id);
   };
 
   return (
@@ -21,13 +23,13 @@ export default function MoveToArchiveModal(props: BaseModalProps<string>) {
       <DeviceBased
         desktop={
           <div style={{ gap: "16px" }}>
-            <MoveToArchive taskName={title} />
+            <MoveToArchive name={title} type={type} />
             <DialogButtons variant="move" onClose={handleClose} onMove={handleMove} />
           </div>
         }
         mobile={
           <div style={{ gap: "24px" }}>
-            <MoveToArchive taskName={title} />
+            <MoveToArchive name={title} type={type} />
             <DialogButtons variant="move" onClose={handleClose} onMove={handleMove} />
           </div>
         }
