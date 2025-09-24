307c30c92a1a6ed56d7600a7402e9dcf0c4f52c3
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-24 17:41:31 +0300

    feat: replace PersonalDataForm with PersonalDataFormMobile in EditPersonalDataPage for mobile optimization

--- src/app/profile/edit-personal-data/page.tsx
+++ src/app/profile/edit-personal-data/page.tsx
@@ -5,33 +5,16 @@ import { BackButton } from "@/presentation/ui/BackButton";
 import { BottomNavBar } from "@/presentation/widgets/dashboard/BottomNavBar";
 import { DashboardHeader } from "@/presentation/widgets/dashboard/Header";
 import { Spacer } from "@/presentation/widgets/primitives/Spacer";
-import { PersonalDataForm } from "@/presentation/widgets/profile/PersonalDataForm/PersonalDataForm";
-import { useUpdateUser } from "@/application/user/hooks/useUpdateUser";
-import { getDisplayName } from "@/shared/utils/getDisplayName";
-
-const personalDataTexts = {
-  userName: "Салунин Максим",
-  userEmail: "skvp138@gmail.com",
-};
+import { PersonalDataFormMobile } from "@/presentation/widgets/profile/PersonalDataForm";
 
 export default function EditPersonalDataPage() {
-  const { user, update } = useUpdateUser();
-
-  const handleSumbit = async (data: { username: string }) => {
-    await update.mutateAsync(data);
-  };
-
   return (
     <MainContainer>
       <DashboardHeader />
       <Spacer size="80px" />
       <BackButton />
       <Spacer size="20px" />
-      <PersonalDataForm
-        initialUsername={getDisplayName(user)}
-        initialEmail={user?.email || personalDataTexts.userEmail}
-        onSubmit={handleSumbit}
-      />
+      <PersonalDataFormMobile />
       <BottomNavBar />
     </MainContainer>
   );
