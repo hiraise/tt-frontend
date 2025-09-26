a552a3d8c2a9708763c0ac9c0eaafa0916d54520
Sergey Rusinovich <rusinovich.s@hiraise.net>
2025-09-26 15:11:02 +0300

    feat: refactor BottomNavBar to use custom MenuItem component and improve styling

--- src/presentation/widgets/dashboard/BottomNavBar/BottomNavBar.tsx
+++ src/presentation/widgets/dashboard/BottomNavBar/BottomNavBar.tsx
@@ -1,24 +1,68 @@
-import { usePathname } from "next/navigation";
+"use client";
 
-import { Label, NavBar, NavItem } from "./BottomNavBar.styled";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AnimatePresence, motion as m } from "framer-motion";
+import clsx from "clsx";
+
+import styles from "./BottomNavBar.module.css";
+
 import { Icon } from "@/presentation/ui/Icon";
-import { navItems } from "./BottomNavBar.config";
+import { NavItem, navItems } from "./BottomNavBar.config";
 import { normalize } from "@/shared/utils/formatters";
+import { ICONS } from "@/infrastructure/config/icons";
+import { ROUTES } from "@/infrastructure/config/routes";
 
 export function BottomNavBar() {
   const pathName = usePathname();
 
   return (
-    <NavBar>
-      {navItems.map((item) => {
-        const isCurrent = normalize(pathName) === normalize(item.href);
-        return (
-          <NavItem key={item.label} href={item.href} $active={isCurrent}>
-            <Icon as={item.icon} size="24px" />
-            <Label>{item.label}</Label>
-          </NavItem>
-        );
-      })}
-    </NavBar>
+    <div className={styles.container}>
+      <Link className={styles.search} href={ROUTES.dashboard}>
+        <Icon as={ICONS.search} size="24px" inheritColor />
+      </Link>
+      <div className={styles.menu}>
+        {navItems.map((item) => {
+          const isCurrent = normalize(pathName) === normalize(item.href);
+          return <MenuItem key={item.label} {...item} isCurrent={isCurrent} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+const MotionLink = m.create(Link);
+
+interface MenuItemsProps extends NavItem {
+  isCurrent: boolean;
+}
+
+function MenuItem({ isCurrent, ...props }: MenuItemsProps) {
+  const { href, icon, label } = props;
+  const iconColor = isCurrent ? "var(--icon-primary)" : "var(--icon-secondary)";
+
+  return (
+    <MotionLink
+      href={href}
+      className={clsx(styles.icon, { [styles.active]: isCurrent })}
+      animate={{ width: isCurrent ? "auto" : 48 }}
+      transition={{ duration: 0.2, ease: "easeInOut" }}
+    >
+      <Icon as={icon} size="24px" color={iconColor} />
+      <AnimatePresence mode="wait">
+        {isCurrent && (
+          <m.span
+            key="label"
+            className="caption-med"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+          >
+            {label}
+          </m.span>
+        )}
+      </AnimatePresence>
+    </MotionLink>
   );
 }
