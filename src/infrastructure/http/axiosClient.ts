1a7d62348f152e4e8e8d7092a8761e05a4bf0a5a
Sergey Rusinovich <rusinovich.s@hiraise.net>
2026-01-06 16:22:17 +0300

    fix(axiosClient): remove unnecessary method call on axiosClient instantiation

--- src/infrastructure/http/axiosClient.ts
+++ src/infrastructure/http/axiosClient.ts
@@ -2,6 +2,6 @@ import { AxiosHttpClient } from "./AxiosHttpClient";
 
 const axiosClient = new AxiosHttpClient(
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
-).getAxiosInstance();
+);
 
 export default axiosClient;
