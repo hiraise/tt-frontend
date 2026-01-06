bfec7cd2354b629b479b74208aeacdc66c4fed80
Sergey Rusinovich <rusinovich.s@hiraise.net>
2026-01-06 16:22:42 +0300

    fix(axiosClient): simplify axiosClient instantiation by removing unnecessary line breaks

--- src/infrastructure/http/axiosClient.ts
+++ src/infrastructure/http/axiosClient.ts
@@ -1,7 +1,5 @@
 import { AxiosHttpClient } from "./AxiosHttpClient";
 
-const axiosClient = new AxiosHttpClient(
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
-);
+const axiosClient = new AxiosHttpClient(process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080");
 
 export default axiosClient;
