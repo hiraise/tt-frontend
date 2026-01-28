a5f4b75133d52768b635c4d04e8867073c68ef86
Sergey Rusinovich <rusinovich.s@hiraise.net>
2026-01-28 15:03:28 +0300

    refactor(repositories): add ApiUserRepository with error handling and API methods

--- src/infrastructure/repositories/index.ts
+++ src/infrastructure/repositories/index.ts
@@ -2,4 +2,4 @@ export { createAuthRepository } from "./apiAuthRepository";
 export { createProjectMemberRepository } from "./apiProjectMemberRepository";
 export { createProjectRepository } from "./apiProjectRepository";
 export { createTaskRepository } from "./apiTaskRepository";
-export { createUserRepository } from "./apiUserRepository123";
+export { createUserRepository } from "./apiUserRepository";
