96642f5536a0fda10682acb774a946e57fe162aa
Sergey Rusinovich <rusinovich.s@hiraise.net>
2026-02-05 14:49:18 +0300

    refactor: move styles to CSS module for ProjectSettingsModal and simplify component structure

--- src/presentation/features/projects/modals/ProjectSettingsModal/ProjectSettingsModal.tsx
+++ src/presentation/features/projects/modals/ProjectSettingsModal/ProjectSettingsModal.tsx
@@ -1,38 +1,11 @@
 import { useParams } from "next/navigation";
-import styled from "styled-components";
 
 import type { ProjectId } from "@/domain/types";
 import { BaseModal, Icon, type BaseModalProps } from "@/presentation/shared";
 
 import { useProjectMenuItems } from "../../hooks/useProjectMenuItems";
 
-const MenuWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-`;
-
-const MenuButton = styled.button`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 12px;
-  padding: 8px 8px 8px 0px;
-  border: none;
-  border-radius: 12px;
-  background-color: transparent;
-  transition: background-color 0.2s;
-  height: 40px;
-
-  svg {
-    margin: -4px;
-  }
-
-  &:hover {
-    cursor: pointer;
-    background-color: var(--bg-secondary-2);
-  }
-`;
+import styles from "./ProjectSettingsModal.module.css";
 
 export function ProjectSettingsModal(props: BaseModalProps<void>) {
   const params = useParams();
@@ -41,9 +14,10 @@ export function ProjectSettingsModal(props: BaseModalProps<void>) {
 
   return (
     <BaseModal {...props} title="">
-      <MenuWrapper>
+      <div className={styles.wrapper}>
         {menuItems.map((item, index) => (
-          <MenuButton
+          <button
+            className={styles.button}
             key={index}
             style={{ color: item.color ? item.color : undefined }}
             onClick={item.onClick}
@@ -52,9 +26,9 @@ export function ProjectSettingsModal(props: BaseModalProps<void>) {
             <span className="btn-font-s" style={{ color: "inherit" }}>
               {item.label}
             </span>
-          </MenuButton>
+          </button>
         ))}
-      </MenuWrapper>
+      </div>
     </BaseModal>
   );
 }
